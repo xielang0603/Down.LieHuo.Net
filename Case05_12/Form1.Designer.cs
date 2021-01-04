0485c4966d443853689ef0049e5b10fc3e4fea3c
lihui tan <lh.tan@163.com>
2021-01-04 16:40:05 +0800

    添加项目文件。

---
+++ Case05_12/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Case05_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(412, 257);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SQL Server数据库数据信息";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(406, 300);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(7, 467);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(686, 54);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "控制按钮";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(304, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "退出程序";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "连接SQL Server数据库";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(571, 190);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 73);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "查询代码";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(561, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(565, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(154, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(565, 161);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(154, 21);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(559, 377);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 90);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 533);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "连接SQL Server数据库";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Case05_12/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Download by http://down.liehuo.net
+using System.Data.SqlClient;
+
+namespace Case05_12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ////生成连接数据库字符串
+                //string ConStr = "server=(192.168.35.206,1433);user id=sa;pwd=123456;database=pxscj";
+
+                ////定义SqlConnection对象实例  
+                //SqlConnection con = new SqlConnection(ConStr);
+
+                ////定义Select查询语句
+                //string SqlStr = "select * from 设备1";
+                //SqlDataAdapter ada = new SqlDataAdapter(SqlStr, con);
+
+                //DataSet ds = new DataSet();    //定义DataSet对象实例
+                //ada.Fill(ds);
+                ////连接数据表格，显示数据  
+
+                //this.dataGridView1.DataSource = ds.Tables[0].DefaultView;
+                //********************************************************************
+                string str = "network address = 192.168.35.206,1433; password = 123456; user id = sa; database = pxscj";
+
+                //定义数据库连接
+                SqlConnection conn = new SqlConnection(str);
+                //进行数据库连接
+                conn.Open();
+                //操作数据库
+
+
+                //定义Select查询语句
+                string SqlStr = "select * from 设备1";
+                SqlDataAdapter ada = new SqlDataAdapter(SqlStr, conn);
+                DataSet ds = new DataSet();    //定义DataSet对象实例
+                ada.Fill(ds);
+                //连接数据表格，显示数据  
+                this.dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+
+                //增一行
+                string sqlStr = "insert into 设备1( 日期1 , 日期2 )values( 999 , 888 )";
+                //执行指定的SQL命令语句,并返回命令所影响的行数
+                SqlCommand comminsert = new SqlCommand(sqlStr, conn);
+
+                //int Succnum = comminsert.executeCommand(sqlStr);
+                int Succnum = comminsert.ExecuteNonQuery();
+                if (Succnum > 0) MessageBox.Show("录入成功");
+
+
+
+                //关闭数据库
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                //********************************************************************
+
+
+            }
+            catch
+            {
+                return;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //一：C#数据库查询之数据库连接代码：
+
+            //SqlConnection objSqlConnection = new SqlConnection("server=192.168.35.206,1433;uid=sa;pwd=123456;database=pxscj");
+            //objSqlConnection.Open();
+
+            // 五：数据库的查询代码：
+
+            //1.类开始：
+
+            DataTable dt1 = new DataTable();
+            SqlDataAdapter da1 = new SqlDataAdapter();
+
+            //2.按钮代码：
+            int i = 0,n = 0;
+            string s1 = "",s2 = "";
+            s1 = textBox2.Text;
+            s2 = textBox3.Text;
+            if (textBox1.Text.Length == 0)
+                i = 0;
+            else
+                i = Convert.ToInt32(textBox1.Text);
+            SqlConnection objSqlConnection = new SqlConnection("server=192.168.35.206,1433;uid=sa;pwd=123456;database=pxscj");
+            objSqlConnection.Open();
+            MessageBox.Show("数据库连接成功", "好");
+
+            string query = "SELECT * from info where id=" + i;
+
+            DataSet objDataSet = new DataSet();
+
+            SqlDataAdapter obj = new SqlDataAdapter();
+
+            obj.SelectCommand = new SqlCommand(query, objSqlConnection);
+
+            obj.Fill(objDataSet, "info");
+
+            SqlCommand objSqlCommand = new SqlCommand(query, objSqlConnection);
+
+            SqlDataReader objSqlReader = objSqlCommand.ExecuteReader();
+
+            while (objSqlReader.Read())
+            {
+                n += 1;
+                MessageBox.Show("编号:" + objSqlReader.GetValue(0) + "姓名:" + objSqlReader.GetValue(1) + "性别" + objSqlReader.GetValue(2));
+            }
+            if (n == 0)
+                MessageBox.Show("数据库中没有这样的记录！");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //C#数据库查询代码：
+
+            DataTable dt1 = new DataTable();
+            SqlDataAdapter da1 = new SqlDataAdapter();
+
+            int i = 0;
+            //intn=0;  
+            string s1 = "",s2 = "";
+            string sql;
+            s1 = textBox2.Text;
+            s2 = textBox3.Text;
+
+            if (textBox1.Text.Length == 0)
+            {
+                i = 0;
+            }
+
+            else
+            {
+                i = Convert.ToInt32(textBox1.Text);
+            }
+
+            SqlConnection objSqlConnection = new SqlConnection("server=192.168.35.206,1433;uid=sa;pwd=123456;database=pxscj");
+            objSqlConnection.Open();
+            //MessageBox.Show("数据库连接成功", "好");
+            string query = "SELECT * from 设备1 where id=日期1" + i;
+            if (i == 0)
+                sql = "select * from 设备1";
+            else
+                sql = "select * from 设备1 where 日期1=" + s1 + "And 日期2=" + s2;
+            da1 = new SqlDataAdapter(sql, objSqlConnection);
+            dt1.Clear();
+            da1.Fill(dt1);
+            dataGridView1.DataSource = dt1;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                ////生成连接数据库字符串
+                //string ConStr = "server=(192.168.35.206,1433);user id=sa;pwd=123456;database=pxscj";
+
+                ////定义SqlConnection对象实例  
+                //SqlConnection con = new SqlConnection(ConStr);
+
+                ////定义Select查询语句
+                //string SqlStr = "select * from 设备1";
+                //SqlDataAdapter ada = new SqlDataAdapter(SqlStr, con);
+
+                //DataSet ds = new DataSet();    //定义DataSet对象实例
+                //ada.Fill(ds);
+                ////连接数据表格，显示数据  
+
+                //this.dataGridView1.DataSource = ds.Tables[0].DefaultView;
+                //********************************************************************
+                string str = "network address = 192.168.35.206,1433; password = 123456; user id = sa; database = pxscj";
+
+                //定义数据库连接
+                SqlConnection conn = new SqlConnection(str);
+                //进行数据库连接
+                conn.Open();
+                //操作数据库
+
+
+                //定义Select查询语句
+                string SqlStr = "select * from 设备1";
+                SqlDataAdapter ada = new SqlDataAdapter(SqlStr, conn);
+                DataSet ds = new DataSet();    //定义DataSet对象实例
+                ada.Fill(ds);
+                //连接数据表格，显示数据  
+                this.dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+
+                //增一行
+                string sqlStr = "insert into 设备1( 日期1 , 日期2 )values( 999, 888 )";
+                //执行指定的SQL命令语句,并返回命令所影响的行数
+                SqlCommand comminsert = new SqlCommand(sqlStr, conn);
+
+                //int Succnum = comminsert.executeCommand(sqlStr);
+                int Succnum = comminsert.ExecuteNonQuery();
+                //if (Succnum > 0) MessageBox.Show("录入成功");
+
+
+
+                //关闭数据库
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                //********************************************************************
+
+
+            }
+            catch
+            {
+                return;
+            }
+        }
+    }
+}
